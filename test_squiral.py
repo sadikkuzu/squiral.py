74c215fca0f4c5bee751c3a5d32d9f11645bec34
SADIK KUZU <sadikkuzu@hotmail.com>
2023-06-18 08:46:07 +0300

    Improve quality

--- test_squiral.py
+++ test_squiral.py
@@ -1,15 +1,17 @@
 import pytest
+
 import squiral
-from testdata_squiral import data_to_where, data_next_point
+from testdata_squiral import data_next_point
+from testdata_squiral import data_to_where
 
 
 @pytest.mark.parametrize("A, direction", data_to_where)
-def test_yn(A, direction):
+def test_to_where(A, direction):
     assert squiral.to_where(A) == direction
 
 
 @pytest.mark.parametrize("row1, col1, direction, row2, col2", data_next_point)
-def test_ist(row1, col1, direction, row2, col2):
+def test_next_point(row1, col1, direction, row2, col2):
     assert squiral.next_point(row1, col1, direction) == (row2, col2)
 
 
