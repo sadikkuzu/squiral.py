7573531dd9b62afd646d040ea6f96c05c910f1f6
SADIK KUZU <sadikkuzu@hotmail.com>
2023-06-18 17:34:32 +0300

    Add upgrader shell script

---
+++ scripts/upgrader.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+python -m pip install --upgrade pip
+pip install pip-tools
+pip-compile -vU requirements-tool.in
+pip install -r requirements-tool.txt
+pip-compile -vU
+pip-compile -vU requirements-dev.in
+pip install -r requirements-dev.txt
+pre-commit autoupdate
+pre-commit run --all-files
+poetry update
